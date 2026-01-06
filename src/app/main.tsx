739295cf928a3d4049283b54a2eabc3149308883
beforedisappear <roman23kl@gmail.com>
2026-01-06 23:06:59 +0300

    add mocked api & ui

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,10 +3,23 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { router } from './router';
 import { RouterProvider } from 'react-router-dom';
+import { Providers } from './providers';
 
+async function enableMocking() {
+  if (import.meta.env.PROD) {
+    return;
+  }
 
+  const { worker } = await import('@/shared/api/mocks/browser');
+  return worker.start();
+}
+
+enableMocking().then(() => {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
+    </StrictMode>,
+  );
+});
